22eecec920f99f74867bac354419bbdd82f4bd9d
Manuel Gomez <manuel.gomez@kodesoft.net>
2013-06-03 00:04:56 +0200

    first working commit. Needs argument error handling

--- src/com/facelift/Main.java
+++ src/com/facelift/Main.java
@@ -20,17 +20,20 @@ public class Main {
             String fileExtensions = "php,js,html";
             //create pattern list
             //TODO load patterns from config file
-            ArrayList<Pattern> patternList = new ArrayList<Pattern>();
-            patternList.add(0, Pattern.compile("(\\{\\{)(\\s*)(\\\"|\\')(.+)(\\\"|\\')(\\s*)(\\|)(\\s*)trans(\\s*)(\\}\\})"));
-            patternList.add(1, Pattern.compile("_\\((\\'|\\\")(.+)(\\'|\\\")\\)"));
+            ArrayList<Pattern> inputPatternList , outputFilterPatternList;
+            inputPatternList = new ArrayList<Pattern>();
+            outputFilterPatternList = new ArrayList<Pattern>();
+            inputPatternList.add(0, Pattern.compile("(\\{\\{)(\\s*)(\\\"|\\')(.+)(\\\"|\\')(\\s*)(\\|)(\\s*)trans(\\s*)(\\}\\})"));
+            inputPatternList.add(1, Pattern.compile("_\\((\\'|\\\")(.+)(\\'|\\\")\\)"));
+            outputFilterPatternList.add(0, Pattern.compile("\\'((\\w|\\.){3,})\\'"));
             //load files to a list
             FileScanner fscanner = new FileScanner(rootDir,fileExtensions);
             //start xscanner engine
             XScanner scanner = new XScanner();
-            scanner.startEngine(fscanner.scanFileList(), patternList).doMagic();
+            scanner.startEngine(fscanner.scanFileList(), inputPatternList, outputFilterPatternList).doMagic();
             //dump results to xliff
             Xliff xliff = new Xliff();
-            xliff.loadStrings(scanner.getOutputList()).dumpToFile(outFileName);
+            xliff.loadStrings(scanner.getFilteredOutputList()).dumpToFile(outFileName);
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/com/facelift/XScanner.java
+++ src/com/facelift/XScanner.java
@@ -9,19 +9,47 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class XScanner {
-    protected List<Pattern> patternList;
+    protected List<Pattern> inputPatternList;
+
+    public List<Pattern> getInputPatternList() {
+        return inputPatternList;
+    }
+
+    public void setInputPatternList(List<Pattern> inputPatternList) {
+        this.inputPatternList = inputPatternList;
+    }
+
+    public List<Pattern> getOutputPatternList() {
+        return outputPatternList;
+    }
+
+    public void setOutputPatternList(List<Pattern> outputPatternList) {
+        this.outputPatternList = outputPatternList;
+    }
+
+    protected List<Pattern> outputPatternList;
     protected List<String> fileList;
     protected List<String> outputList;
 
-
-    public List<Pattern> getPatternList() {
-        return patternList;
+    public List<String> getFilteredOutputList() {
+        for(int i=0; i<getOutputList().size(); i++) {
+            String item = getOutputList().get(i);
+            Pattern pattern = null;
+            Iterator<Pattern> it = getOutputPatternList().iterator();
+            while (it.hasNext()) {
+                pattern = it.next();
+                Matcher m = pattern.matcher(item);
+                System.out.println(m.pattern().toString());
+                if (m.find()) {
+                    item = m.group();
+                    System.out.println(m.group());
+                }
+            }
+            item = item.replace("'","").replace('"',' ').replace(" ","");
+            getOutputList().set(i, item);
+        }
+        return getOutputList();
     }
-
-    public void setPatternList(List<Pattern> patternList) {
-        this.patternList = patternList;
-    }
-
 
     public List<String> getFileList() {
         return fileList;
@@ -50,12 +78,13 @@ public class XScanner {
 
     }
 
-    public XScanner startEngine(List<String> fileList, List<Pattern> patternList) throws Exception {
-        if (patternList.size() == 0) {
+    public XScanner startEngine(List<String> fileList, List<Pattern> inputPatternList, List<Pattern> outputPatternList) throws Exception {
+        if (inputPatternList.size() == 0) {
             throw new Exception();
         }
         this.setFileList(fileList);
-        this.setPatternList(patternList);
+        this.setInputPatternList(inputPatternList);
+        this.setOutputPatternList(outputPatternList);
         this.setOutputList(new ArrayList<String>());
         return this;
     }
@@ -70,7 +99,7 @@ public class XScanner {
     }
 
     private void loopThroughPatterns(String fileContent) throws Exception {
-        Iterator<Pattern> patIterator = getPatternList().iterator();
+        Iterator<Pattern> patIterator = getInputPatternList().iterator();
         while (patIterator.hasNext()) {
             Pattern pattern = patIterator.next();
             addMatchesListToOutputList(fileScan(fileContent, pattern));
@@ -89,6 +118,7 @@ public class XScanner {
     }
 
     private void addMatchesListToOutputList(List<String> matchesList) throws Exception {
+
         Iterator<String> it = matchesList.iterator();
         while (it.hasNext()) {
             addToOutputList(it.next());
